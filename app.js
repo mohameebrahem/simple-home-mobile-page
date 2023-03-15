c8d956719d50795cb0ababee295f6e9ac3b32623
mohamedEbrahem <mohamedebrahemprogrammer@gmail.com>
2023-03-15 16:07:12 +0200

    creating html , css and js files

---
+++ app.js
@@ -0,0 +1,8 @@
+const img = document.querySelector('.img');
+const container = document.querySelector('.container');
+function phones(phone) {
+  img.src = phone;
+}
+function colors(color) {
+  container.style.backgroundColor = color;
+}
